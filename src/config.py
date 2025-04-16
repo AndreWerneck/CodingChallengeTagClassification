23fd75dfda8229f19f4eedb1f5bb94e91b957d2d
AndreWerneck <andrecw97@gmail.com>
2025-04-17 01:09:00 +0200

    add config file for source code

---
+++ src/config.py
@@ -0,0 +1,14 @@
+
+FOCUS_TAGS = [
+    'games', 'geometry', 'graphs', 'math', 'number theory', 
+    'probabilities', 'strings', 'trees'
+]
+
+# Paths
+DATA_PATH = "data/code_classification_dataset_v3.csv"
+TRAIN_OUTPUT_PATH = "data/train_data.csv"
+TEST_OUTPUT_PATH = "data/test_data.csv"
+# Seed
+RANDOM_SEED = 42
+# CV
+N_FOLDS = 3
